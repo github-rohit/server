1094ec87cb3e79a13c1094f451ecd06b75f31d97
github-rohit <nirmal.rohit@yahoo.co.in>
2018-10-13 22:24:59 +0530

    Update middleware/auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,16 +3,11 @@ const jwt = require('jsonwebtoken');
 module.exports = function(req, res, next) {
   const token = req.cookies['x-auth'];
 
-  if (!token) {
-    console.log('x-auth', token);
-    res.status(401).send({
-      errors: {
-        msg: 'Access denied.'
-      }
-    });
-  }
-
   try {
+
+    if (!token) {
+      throw new Error(401);
+    }
     const decoded = jwt.verify(token, process.env.SECRET);
     req.user = decoded;
     next();
